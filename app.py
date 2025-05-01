10ace8a0f230b33fe57ec15fbffe825cb274f321
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-01 14:39:51 +1200

    did some code

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+#made by harrison - numbers go brrrr
+#imports
 import sqlite3
 
-db = sqlite3.connect('table2.db')
-cursor = db.cursor()
-sql = "select * from number_ratings;"
-cursor.execute(sql)
-results = cursor.fetchall()
+#constants and varibles
+DATABASE = "table2.db"
 
-print(results)
+# functions
+def print_all():
+    """prints all number ratings nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from number_ratings;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loops through all results
+    for number in results:
+        print(results)
+    #end of loop
+
+#main code
+print_all()
 
 db.close()
